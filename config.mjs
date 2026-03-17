712d7e769ec723b949aceda4d24d1066c33204ed
shustinedominiquie-web <shustinedominiquie@gmail.com>
2026-03-16 23:10:48 -0700

    Update RAM value from 3009 to 3008

--- config.mjs
+++ config.mjs
@@ -5,6 +5,6 @@
 export const REGION = "us-east-1";
 
 export const SITE_NAME = "content-engine";
-export const RAM = 3009;
+export const RAM = 3008;
 export const DISK = 2048;
 export const TIMEOUT = 240;
